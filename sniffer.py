5ba161b2bff490b67cf4f430168e76faf90e7eaa
Joshua gordon <jodogo9897@gmail.com>
2023-06-10 02:33:10 +0300

    sniffer updated dose not work well on file 	new file:   Labsetup/docker-compose.yml 	modified:   sniffer.py

--- sniffer.py
+++ sniffer.py
@@ -1,69 +1,134 @@
-from scapy.all import sniff, TCP, IP
+from scapy.all import sniff, TCP, UDP, ICMP, IP
 import datetime
 
-
-
-
+DEFAULT_SERVER_PORT = 9999  # The default port for the server
+DEFAULT_PROXY_PORT = 9998  # The default port for the proxy
 ID_1 = 332307073
 ID_2 = 214329633
 
 class PacketSniffer:
-    def __init__(self, server_port, proxy_port):
-        self.file_name = str(ID_1)+"_"+str(ID_2)
-        #self.CLIENT_PORT = client_port
-        self.SERVER_PORT = server_port
-        self.PROXY_PORT = proxy_port
-        
+    def __init__(self, proto):
+        self.file_name = f"{ID_1}_{ID_2}.txt"
+        self.protocol = proto
+
     def sniff_packets(self):
-        sniff(filter="tcp", prn=self.process_packet)
+        sniff(filter=self.protocol, prn=self.process_packet, store=False)
 
     def process_packet(self, packet):
-        if TCP in packet and (packet[TCP].sport in [ self.SERVER_PORT, self.PROXY_PORT] or packet[TCP].dport in [ self.SERVER_PORT, self.PROXY_PORT]):
-            # Extract relevant information from the packet
-            source_ip = packet[IP].src
-            dest_ip = packet[IP].dst
-            source_port = packet[TCP].sport
-            dest_port = packet[TCP].dport
-            timestamp = str(datetime.datetime.now())
-            total_length = packet[IP].len
-            cache_flag = packet[TCP].flags & TCP_FLAG_PUSH != 0
-            steps_flag = packet[TCP].flags & TCP_FLAG_ACK != 0
-            type_flag = packet[TCP].flags & TCP_FLAG_URG != 0
-            status_code = packet[TCP].flags & TCP_FLAG_RST != 0
-            cache_control = packet[TCP].flags & TCP_FLAG_SYN != 0
-            data = packet[TCP].payload
+        parsed_data = {}
 
-            # Prepare the packet information in the desired format
-            packet_info = {
-                "source_ip": source_ip,
-                "dest_ip": dest_ip,
-                "source_port": source_port,
-                "dest_port": dest_port,
-                "timestamp": timestamp,
-                "total_length": total_length,
-                "cache_flag": cache_flag,
-                "steps_flag": steps_flag,
-                "type_flag": type_flag,
-                "status_code": status_code,
-                "cache_control": cache_control,
-                "data": data.hex()  # Convert data to hexadecimal
-            }
+        if self.protocol == "tcp":
+            if TCP in packet:
+                parsed_data = self.parse_tcp_packet(packet)
+        elif self.protocol == "udp":
+            if UDP in packet:
+                parsed_data = self.parse_udp_packet(packet)
+        elif self.protocol == "icmp":
+            if ICMP in packet:
+                parsed_data = self.parse_icmp_packet(packet)
+        elif self.protocol == "igmp":
+            if IGMP in packet:
+                parsed_data = self.parse_igmp_packet(packet)
+        else:
+            parsed_data = self.parse_raw_packet(packet)
 
-            # Write the packet information to the output file
-            with open(self.file_name, "a") as f:
-                f.write(str(packet_info) + "\n")
+        if parsed_data:
+            self.save_packet_data(parsed_data)
+
+    def parse_tcp_packet(self, packet):
+        parsed_data = {
+            "source_ip": packet[IP].src,
+            "dest_ip": packet[IP].dst,
+            "source_port": packet[TCP].sport,
+            "dest_port": packet[TCP].dport,
+            "timestamp": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            "total_length": len(packet),
+            "cache_flag": packet[TCP].flags.C,
+            "steps_flag": packet[TCP].flags.S,
+            "type_flag": packet[TCP].flags.F,
+            "status_code": packet[TCP].flags.A,
+            "cache_control": packet[TCP].flags.P,
+            "data": packet[TCP].payload
+        }
+
+        
+        return parsed_data
+
+    def parse_udp_packet(self, packet):
+        parsed_data = {
+            "source_ip": packet[IP].src,
+            "dest_ip": packet[IP].dst,
+            "source_port": packet[UDP].sport,
+            "dest_port": packet[UDP].dport,
+            "timestamp": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            "total_length": len(packet),
+            "cache_flag": "",
+            "steps_flag": "",
+            "type_flag": "",
+            "status_code": "",
+            "cache_control": "",
+            "data": packet[UDP].payload.hex()
+        }
+        return parsed_data
+
+    def parse_icmp_packet(self, packet):
+        parsed_data = {
+            "source_ip": packet[IP].src,
+            "dest_ip": packet[IP].dst,
+            "source_port": "",
+            "dest_port": "",
+            "timestamp": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            "total_length": len(packet),
+            "cache_flag": "",
+            "steps_flag": "",
+            "type_flag": "",
+            "status_code": "",
+            "cache_control": "",
+            "data": packet[ICMP].payload.hex()
+        }
+        return parsed_data
+
+    def parse_igmp_packet(self, packet):
+        parsed_data = {
+            "source_ip": packet[IP].src,
+            "dest_ip": packet[IP].dst,
+            "source_port": "",
+            "dest_port": "",
+            "timestamp": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            "total_length": len(packet),
+            "cache_flag": "",
+            "steps_flag": "",
+            "type_flag": "",
+            "status_code": "",
+            "cache_control": "",
+            "data": packet[IGMP].payload.hex()
+        }
+        return parsed_data
+
+    def parse_raw_packet(self, packet):
+        parsed_data = {
+            "source_ip": packet[IP].src,
+            "dest_ip": packet[IP].dst,
+            "source_port": "",
+            "dest_port": "",
+            "timestamp": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            "total_length": len(packet),
+            "cache_flag": "",
+            "steps_flag": "",
+            "type_flag": "",
+            "status_code": "",
+            "cache_control": "",
+            "data": packet.payload.hex()
+        }
+        return parsed_data
+
+    def save_packet_data(self, parsed_data):
+        with open(self.file_name, "a") as file:
+            file.write(str(parsed_data) + "\n")
 
 if __name__ == "__main__":
     print("Started")
-    DEFAULT_SERVER_PORT = 9999 # The default port for the server
-    DEFAULT_PROXY_PORT = 9998 # The default port for the proxy
 
-    #cp = DEFAULT_SERVER_PORT
-
-    sp = DEFAULT_SERVER_PORT
-    pp = DEFAULT_PROXY_PORT
-    sniffer = PacketSniffer(sp,pp)
+    sniffer = PacketSniffer("tcp")
     sniffer.sniff_packets()
-    print("here")
-    
-
+    print("Finished")
